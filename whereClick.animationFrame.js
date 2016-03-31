710c736b44a588dd91a0cedaf4b681f41216d4eb
Hoban, Shane <Shane.Hoban@fmr.com>
2016-03-31 11:21:00 +0100

    initial commit

---
+++ whereClick.animationFrame.js
@@ -0,0 +1,64 @@
+	
+	var whereClick = {};
+
+		whereClick.mouseDown = false;
+		whereClick.xCords = 0;
+		whereClick.yCords = 0;
+		whereClick.color = 'red';
+
+		whereClick.bodyElement = document.querySelector('body');
+		whereClick.newElement;
+	
+	whereClick.startDrawing = function(){
+		if(whereClick.mouseDown){
+			window.requestAnimationFrame(whereClick.startDrawing);
+
+				whereClick.newElement = document.createElement('div');
+					whereClick.newElement.style['display'] = 'block';
+					whereClick.newElement.style['height'] = '10px';
+					whereClick.newElement.style['width'] = '10px';
+					whereClick.newElement.style['background'] = whereClick.color;
+					whereClick.newElement.style['position'] = 'absolute';
+					whereClick.newElement.style['left'] = whereClick.xCords + 'px';
+					whereClick.newElement.style['top'] = whereClick.yCords + 'px';
+					whereClick.newElement.style['border-radius'] = '50%';
+
+				whereClick.bodyElement.appendChild(whereClick.newElement);
+		}
+	}
+
+  	document.addEventListener('mousemove', function(event){
+    	whereClick.xCords = event.pageX 
+    	whereClick.yCords = event.pageY;
+  	}); 
+
+  	document.addEventListener('click', function(event){
+		event.preventDefault();
+	});
+
+	document.addEventListener('mousedown', function(event){
+		if(event.ctrlKey){
+			whereClick.color = 'blue';
+		} else if(event.shiftKey){
+			whereClick.color = 'green';
+		} else {
+			whereClick.color = 'red';
+		}
+		whereClick.mouseDown = true;
+		whereClick.startDrawing();
+	});
+
+	document.addEventListener('mouseup', function(event){ 
+		whereClick.mouseDown = false;
+	});
+
+	var allElements = document.querySelectorAll('*');
+	
+	Array.prototype.forEach.call(allElements, function(el, i){
+		el.style['-webkit-touch-callout'] = 'none';
+		el.style['-webkit-user-select'] = 'none';
+		el.style['-khtml-user-select'] = 'none';
+		el.style['-moz-user-select'] = 'none';
+		el.style['-ms-user-select'] = 'none';
+		el.style['user-select'] = 'none';
+	});
---
+++ whereClick.jQuery.js
@@ -0,0 +1,50 @@
+	
+	var whereClick = {};
+
+		whereClick.mouseDown = false;
+		whereClick.xCords = 0;
+		whereClick.yCords = 0;
+		whereClick.color = "red";
+
+	whereClick.startDrawing = function(){
+		if(whereClick.mouseDown){
+			setTimeout(function(){
+				$('body').append('<div class="shanes-thing" style="display:block; height: 10px; width: 10px; background: ' + whereClick.color + '; position: absolute; left: ' + whereClick.xCords + 'px; top: ' + whereClick.yCords + 'px; border-radius: 50%"></div>');
+				whereClick.startDrawing();
+			}, 1);
+		}
+	}
+
+  	$(document).mousemove(function(e){
+    	whereClick.xCords = e.pageX 
+    	whereClick.yCords = e.pageY;
+  	}); 
+
+  	$(document).on('click', function(event){
+		event.preventDefault();
+	});
+
+	$(document).on('mousedown', function(event){
+		if(event.ctrlKey){
+			whereClick.color = "blue";
+		} else if(event.shiftKey){
+			whereClick.color = "green";
+		} else {
+			whereClick.color = "red";
+		}
+		whereClick.mouseDown = true;
+		whereClick.startDrawing();
+	});
+
+	$(document).on('mouseup', function(event){ 
+		whereClick.mouseDown = false;
+	});
+
+	$('*').css({
+		'-webkit-touch-callout': 'none',
+	    '-webkit-user-select': 'none',
+	    '-khtml-user-select': 'none',
+	    '-moz-user-select': 'none',
+	    '-ms-user-select': 'none',
+	    'user-select': 'none'
+	});
